9081fdfc562f078ee1c4a4230d8ce2d215d50573
JP Reinold <rhinojon@gmail.com>
2019-12-26 16:41:01 -0500

    Stopped using visualize global parameters to make the vizualize function more dynamic

--- sketch.js
+++ sketch.js
@@ -1,24 +1,13 @@
-let brain;
+let population; let test; let output;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	frameRate(1);
-	brain = new Brain(8, 4);
-	nodeRadius = windowHeight / 20;
-	nodePadding = windowHeight / 15;
-	visualizeX = 0;
-	visualizeY = 0;
-	visualizeWidth = windowWidth;
-	visualizeHeight = windowHeight;
+	population = new Population(8, 4);
+	test = [0.1, .5, .75, .2, .23, .98, .75, .384];
 }
 
 function draw() {
-	console.log(brain.toString());
-	background(225);
-	brain.visualize(0, 0, windowWidth, windowHeight);
-	if(frameCount == 2){
-		brain.mutateAddConnection();
-	} else if (frameCount == 3){
-		brain.mutateAddNode();
-	}
+	background(100);
+	population.visualize();
 }
